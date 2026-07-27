7d1fd7d15d6aae6c3f0ab2101e65b3c50011c0bf
mikebrucker <disposition11@gmail.com>
2026-07-27 20:12:00 +0200

    footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -10,13 +10,13 @@ function Footer({ className, sticky = false, ...props }: FooterProps) {
     <footer
       data-slot="footer"
       className={cn(
-        "flex w-full items-center justify-around bg-card border-t rounded-t-lg px-4 py-4 shadow-[0_-5px_6px_-1px_rgb(0_0_0/0.25),0_-3px_4px_-2px_rgb(0_0_0/0.15)] dark:shadow-[0_-5px_6px_-1px_rgb(0_0_0/0.35),0_-3px_4px_-2px_rgb(0_0_0/0.22)]",
+        "flex w-full items-center justify-around bg-card border-t rounded-t-lg px-4 py-2 shadow-[0_-5px_6px_-1px_rgb(0_0_0/0.25),0_-3px_4px_-2px_rgb(0_0_0/0.15)] dark:shadow-[0_-5px_6px_-1px_rgb(0_0_0/0.35),0_-3px_4px_-2px_rgb(0_0_0/0.22)]",
         sticky && "sticky bottom-0 z-10",
         className,
       )}
       {...props}
     >
-      <div className="flex font-metal-mania text-lg tracking-widest text-shadow-md text-shadow-theme-600 dark:text-shadow-theme-400">
+      <div className="flex font-metal-mania text-base sm:text-lg tracking-widest text-shadow-md text-shadow-theme-600 dark:text-shadow-theme-400 transition-all">
         <span>&copy; {new Date().getFullYear()}&nbsp;&nbsp;~&nbsp;&nbsp;Mike Brucker</span>
       </div>
     </footer>
